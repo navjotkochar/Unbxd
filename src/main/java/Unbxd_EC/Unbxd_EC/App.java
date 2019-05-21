d28c4e46e76467105265d584d8e565c7604de4f7
navjotkochar <navjot.beri@gmail.com>
2019-05-21 14:48:06 +0530

    First Commit

---
+++ src/main/java/Unbxd_EC/Unbxd_EC/App.java
@@ -0,0 +1,13 @@
+package Unbxd_EC.Unbxd_EC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
